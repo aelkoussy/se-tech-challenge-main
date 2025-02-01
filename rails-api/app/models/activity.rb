17260d599a419414397a54f4621a129d6d067240
Ahmed Elkoussy <aaelkoussy@gmail.com>
2025-02-01 12:10:32 +0100

    add meilisearch to the models activitiy and supplier

--- rails-api/app/models/activity.rb
+++ rails-api/app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ApplicationRecord
+  include MeiliSearch::Rails
   belongs_to :supplier
 
   validates :title, :price, :currency, presence: true
@@ -7,4 +8,11 @@ class Activity < ApplicationRecord
               greater_than_or_equal_to: 0,
               less_than_or_equal_to: 5
             }
+
+  meilisearch do
+    # all attributes will be sent to Meilisearch if block is left empty
+    displayed_attributes %i[title price currency rating specialOffer]
+    searchable_attributes %i[title price currency rating]
+    filterable_attributes [:specialOffer]
+  end
 end
--- rails-api/app/models/supplier.rb
+++ rails-api/app/models/supplier.rb
@@ -1,5 +1,13 @@
 class Supplier < ApplicationRecord
+  include MeiliSearch::Rails
   has_many :activities, dependent: :destroy
 
   validates :name, presence: true
+
+  meilisearch do
+    # all attributes will be sent to Meilisearch if block is left empty
+    displayed_attributes %i[name address zip city country]
+    searchable_attributes %i[name address zip city country]
+    filterable_attributes %i[name address zip city country]
+  end
 end
